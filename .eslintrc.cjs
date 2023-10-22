53ad3c562105bb1b5010dea5a11000a493ac5955
Conrad Buck <conartist6@gmail.com>
2023-10-22 10:16:35 -0600

    commonjs-ify

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,11 @@
-module.exports = require('@bablr/eslint-config-base');
+const base = require('@bablr/eslint-config-base');
+
+module.exports = {
+  ...base,
+  globals: {
+    ...base.globals,
+    require: 'readonly',
+    module: 'readonly',
+    Proxy: 'readonly',
+  },
+};
